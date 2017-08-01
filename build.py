b5228a2d9e5386765818eff2d1fd3ada7b2d1e98
Matt Sturgeon <matt@sturgeon.me.uk>
2017-08-01 18:41:27 +0100

    Refactor args, work on HTML and CSS
    Moved arg sanatisation into parse_args(). Count the number of stdin/
    stdout args instead of manually checking each.
    
    Add options for outputting css and controlling how it is added to HTML.
    TODO: Actually add it to HTML
    
    Actually write html to args.html. Also write css to args.css

--- build.py
+++ build.py
@@ -18,14 +18,40 @@ def parse_args():
     parser.add_argument('input', type=infile,
         help='The markdown file to use as input (use - for stdin)')
     parser.add_argument('-s', '--style', type=infile, default=False,
-        help='The css file to use (use - for stdin)')
+        help='The scss file to use (use - for stdin)')
     parser.add_argument('--html', type=outfile, default=False,
         help='Where to build the html output (use - for stdout)')
     parser.add_argument('-p', '--pdf', type=outfile, default=False,
         help='Where to build the pdf output (use - for stdout)')
+    parser.add_argument('-c', '--css', type=outfile, default=False,
+        help='Optional file to output CSS into')
+    parser.add_argument('-i', '--inline', action='store_true', default=False,
+        help='In HTML, include CSS within a <style> tag instead of linking to --css')
+    parser.add_argument('--no-css', action='store_true', default=False,
+        help='Prevent --css from adding CSS to --html')
     # parser.add_argument('-w', '--watch', action='store_true', default=False, help='Enable watching for changes')
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    # Count how many args equal stdin or stdout
+    stdin_count = 0
+    stdout_count = 0
+    for arg in vars(args):
+        if arg == stdin: stdin_count += 1
+        elif arg == stdout: stdout_count += 1
+
+    # Ensure we have at least one output
+    if not args.pdf and not args.html and not css:
+        exit('You must specify at least one output (--html, --pdf or --css)!')
+    # Ensure we only use stdin for one input
+    if stdin_count > 1:
+        exit('You can only use stdin for one thing (input or --style)!')
+    # Ensure we only use stdout for one output
+    if stdout_count > 1:
+        exit('You can only print stdout to one output (--html or --pdf)!')
+
+    return args
+
 
 def sass(infile):
     return compile_scss(infile.read())
@@ -37,21 +63,11 @@ def pdf(outfile, html='', css=''):
     font_config = FontConfiguration()
     HTML(string=html).write_pdf(outfile.buffer, stylesheets=[CSS(string=css, font_config=font_config)])
 
-def main():
+def main(args):
 
-    # Parse arguments
-    args = parse_args()
-
-    # Ensure we have at least one output
-    if not args.pdf and not args.html:
-        exit('You must specify at least one output (--html or --pdf)!')
-    # Ensure we only use stdin for one input
-    if args.input == stdin and args.style == stdin:
-        exit('You can only use stdin for one thing (input or --style)!')
-    # Ensure we only use stdout for one output
-    if args.html == stdout and args.pdf == stdout:
-        exit('You can only print stdout to one output (--html or --pdf)!')
-
+    print(args)
+    print()
+    print()
 
     html = markdown(args.input)
 
@@ -59,10 +75,20 @@ def main():
     if args.style:
         css = sass(args.style)
 
+    if args.css:
+        args.css.write(css)
+
     if args.html:
-        print('building html') # TODO
+        # TODO: Add options to inject css as <style> or <link>
+        if not args.no_css:
+            if args.inline:
+                print('inline')
+            elif args.css:
+                print('linking')
+        else: print('no-css!!')
+        args.html.write(html)
 
     if args.pdf:
         pdf(args.pdf, html, css)
 
-main()
+main(parse_args())
