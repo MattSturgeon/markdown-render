21ddf32237c2d28ab5992a38e4cf48fd20d6f996
Matt Sturgeon <matt@sturgeon.me.uk>
2024-06-04 04:07:58 +0100

    Add python setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='python-resume',
+      version='1.0',
+      packages=find_packages(),
+      scripts=["build.py"],
+     )
